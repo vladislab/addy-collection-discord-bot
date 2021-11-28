3d80411fe716f03ac4f3ddd87d11a5bd061dea0d
Viet Nguyen <vmnguyen@ryerson.ca>
2021-11-28 10:55:55 -0500

    remove aws secrets

--- index.js
+++ index.js
@@ -3,46 +3,16 @@ require('dotenv').config();
 const AWS = require('aws-sdk');
 const { Client, Intents } = require('discord.js');
 
-// Create a Secrets Manager client
-const region = 'ca-central-1';
-const secretName =
-  'arn:aws:secretsmanager:ca-central-1:976115811350:secret:chum-wl-discord-bot-SZIdgX';
-let secret = '';
-let decodedBinarySecret = '';
-const clientSM = new AWS.SecretsManager({
-  region: region,
-});
-
-// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-// We rethrow the exception by default.
-clientSM.getSecretValue({ SecretId: secretName }, function (err, data) {
-  if (err) {
-    console.error(err);
-  } else {
-    // Decrypts secret using the associated KMS CMK.
-    // Depending on whether the secret is a string or binary, one of these fields will be populated.
-    if ('SecretString' in data) {
-      secret = data.SecretString;
-    } else {
-      let buff = new Buffer(data.SecretBinary, 'base64');
-      decodedBinarySecret = buff.toString('ascii');
-    }
-    console.log(secret, decodedBinarySecret);
-  }
-});
-
 // Update our AWS Connection Details
 AWS.config.update({
-  region: secret['AWS_DEFAULT_REGION'] || process.env.AWS_DEFAULT_REGION,
-  accessKeyId: secret['AWS_ACCESS_KEY_ID'] || process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey:
-    secret['AWS_SECRET_ACCESS_KEY'] || process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_DEFAULT_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 // Create the service used to connect to DynamoDB
 const docClient = new AWS.DynamoDB.DocumentClient();
 // Setup the parameters required to save to Dynamo
-const TableName = secret['AWS_TABLE_NAME'] || process.env.AWS_TABLE_NAME;
+const TableName = process.env.AWS_TABLE_NAME;
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -97,7 +67,7 @@ function store(whitelistObj, callback) {
   });
 }
 
-const discord_token = secret['DISCORD_TOKEN'] || process.env.DISCORD_TOKEN;
+const discord_token = process.env.DISCORD_TOKEN;
 // Register an event so that when the bot is ready, it will log a messsage to the terminal
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -142,4 +112,4 @@ client.on('message', async (msg) => {
 });
 
 // client.login logs the bot in and sets it up for use. You'll enter your token here.
-// client.login(discord_token);
+client.login(discord_token);
